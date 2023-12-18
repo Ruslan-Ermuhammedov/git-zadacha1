2f6d6b6d7ad487d0b29a4f4dcd09ef08b55d9387
Ruslan-Ermuhammedov <ermuhammedovruslan18@gmail.com>
2023-12-18 19:30:11 +0500

    feat-create-login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      <button>enter</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default Login
